8473db40374a0a5993fa1bc13d8bc7db4b236be7
DS4506 <davidsmith4506@gmail.com>
2024-01-02 21:25:40 -0600

    updat2

--- register.js
+++ register.js
@@ -11,33 +11,45 @@ let petSalon = {
         open:"9:00 am",
         close:"8:00 pm"
     },
-    pets:[
-        {
-            name:"daffy",
-            age:25,
-            gender:"Male",
-            service:"Grooming"
-        },
-        {
-            name:"Scooby",
-            age:35,
-            gender:"Male",
-            service:"Nail cut",
-        },
-        {
-            name:"Tweety",
-            age:79,
-            gender:"Male",
-            service:"Vaccine"
-        }
-    ]
+    pets:[]
 }
 
+// object constructor (function)
+function pet(name,age,gender,breed,service){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
+}
 
-console.table(petSalon.pets);
-console.log(petSalon.pets[0].name);
-console.log(petSalon.pets[1].name);
-console.log(petSalon.pets[2].name);
+function register(){
+    console.log("Registering");
+    // get the values from the inputs
+    let inputName= document.
+    getElementById("txtName").value;
+    let inputAge = document.
+    getElementById("txtAge").value;
+    let inputGender = document.
+    getElementById("txtGender").value;
+    let inputBreed = document.
+    getElementById("txtBreed").value;
+    let inputService = document.
+    getElementById("txtService").value;
 
-// display names
-console.log
+    
+    // Creating the obj
+    let newPet = new pet(inputName,inputAge,inputGender,inputBreed,inputService);
+ //push the object
+ petSalon.pets.push(newPet);
+ // display the pets array on the console
+ console. log(petSalon.pets);
+}
+
+// creating pets using constructor
+let p1 = new pet("Scooby",60,"Male","Pittbull","Haircut");
+let p2 = new pet("Jerome",50,"Male","Frenchy","Nail trim");
+let p3 = new pet("Keshia",45,"Female","Germansheperad","Massage");
+//pushing pets into the pets arrary
+petSalon.pets.push(p1,p2,p3);
+console.log(petSalon.pets);
